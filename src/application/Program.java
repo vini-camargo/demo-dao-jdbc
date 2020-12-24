94ae5184d1336c264e8e60e0f4dc12fa63338011
Vinicius Camargo <vicamargofranca@gmail.com>
2020-12-24 02:02:08 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -25,6 +26,10 @@ public class Program {
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
 		
+		System.out.println("\n=== TEST 4: seller insert ====");
+		Seller newSeller = new Seller(1, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 
 }
